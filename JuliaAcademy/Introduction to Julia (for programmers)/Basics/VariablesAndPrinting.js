1c46c299bbece2e5c05486c249e357140aa43623
Camila Braz <camilabraz03@gmail.com>
2022-04-23 16:57:40 -0300

    Create VariablesAndPrinting.js

---
+++ JuliaAcademy/Introduction to Julia (for programmers)/Basics/VariablesAndPrinting.js
@@ -0,0 +1,27 @@
+println("Hello Julia")
+
+# Variable assignment
+# dynamic assignment
+ans = 42
+typeof(ans)
+
+pi = 3.14159
+typeof(pi)
+
+str = "Julia is awesome"
+typeof(str)
+
+😸 = "An emoji variable"
+
+# Comments are made using hashtags
+
+# Basic math
+sum = 11 + 3
+difference = 15 - 4
+product = 20 * 5
+quotient = 7/9
+power = 10 ^ 2
+modulus = 101 % 2
+
+
+# Exercise
